dc48a94651238f75e978e0f9e850b452afe01933
OnSamuelxd <ondruseksamko@gmail.com>
2026-09-10 11:08:53 +0200

    naučili sme sa používať print yippee

--- 01_úvod/index.php
+++ 01_úvod/index.php
@@ -1,3 +1,28 @@
-<?php
-echo "Ahoj";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "toto je v uvodzovkách";
+    ?>
+
+    <div>
+        <p>nazdar škola</p>
+    </div>
+
+    <?php 
+        echo "toto je ďalšie echo";
+    ?>
+
+    <?php
+        echo "<p> tento kód je vnútro php</p>";
+
+        print("aj toto je moj vyois pomocou funkcie print");
+    ?>
+
+</body>
+</html>
